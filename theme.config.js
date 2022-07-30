fce3ba9bf155a8e43808d7b57ac644e74ef45362
shiyd <50910401+shiiiiyd@users.noreply.github.com>
2022-07-30 11:21:12 +0800

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -3,7 +3,7 @@ const YEAR = new Date().getFullYear()
 export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
-      <time>{YEAR}</time> © sh;;;;yd.
+      <time>{YEAR}</time> © document
       <a href="/feed.xml">RSS</a>
       <style jsx>{`
         a {
